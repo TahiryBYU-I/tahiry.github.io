08eb3974aac8cbac344134c7179f0701e4941a26
Tahiry25 <kamaray.za@gmail.com>
2019-06-10 13:16:36 -0600

    finish preston 6

---
+++ assignments/lesson-6/script/hamburger.js
@@ -0,0 +1,8 @@
+function toggle() {
+  var x = document.getElementById("menuItems");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
