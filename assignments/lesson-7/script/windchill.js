5768c6048b2bcc6561d4faf2f2382ac377bf66f4
Tahiry25 <kamaray.za@gmail.com>
2019-06-11 13:37:11 -0600

    update preston 7 with flex display and add windchill.js

---
+++ assignments/lesson-7/script/windchill.js
@@ -0,0 +1,3 @@
+/* The formula to calculate the wind chill factor is LaTeX: f=35.74+0.6215\:t-35.75\:s^{0.16}+0.4275\:t\:s^{0.16}
+f = 35.74 + 0.6215 t − (35.75 s ^ 0.16) + (0.4275 t s ^ 0.16)
+, where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour. */
