b07b6d9bdf5ddde784b1c19ffd7051e009863b85
Tahiry25 <kamaray.za@gmail.com>
2019-06-11 14:12:18 -0600

    update windchill calculator

--- assignments/lesson-7/script/windchill.js
+++ assignments/lesson-7/script/windchill.js
@@ -1,3 +1,22 @@
 /* The formula to calculate the wind chill factor is LaTeX: f=35.74+0.6215\:t-35.75\:s^{0.16}+0.4275\:t\:s^{0.16}
 f = 35.74 + 0.6215 t − (35.75 s ^ 0.16) + (0.4275 t s ^ 0.16)
 , where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour. */
+
+function getTemp(){
+  x = parseInt(document.getElementById('temperature').innerHTML);
+  return x;
+}
+var t = getTemp();
+
+function getWindSpeed(){
+  x = parseInt(document.getElementById('windspeed').innerHTML);
+  return x;
+}
+var s = getWindSpeed();
+
+function getWindChill(temp,wind){
+  x = (0.0817*(3.71*(Math.pow(wind, 0.5))+5.81-0.25*wind)*(temp-91.4)+91.4);
+  document.getElementById('windchill').innerHTML = parseInt(x);
+}
+
+getWindChill(t,s);
